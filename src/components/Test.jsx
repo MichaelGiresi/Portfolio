a2206c85c34242642fecdd1eda3d46661c8b122f
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2022-06-13 21:16:27 -0400

    Add files via upload

---
+++ src/components/Test.jsx
@@ -0,0 +1,127 @@
+import React, {useState, useEffect} from 'react'
+import test from '../scss/test/test.css'
+
+const Test = () => {
+
+    let [triangleState, setTriangleState] = useState(false)
+
+    console.log(triangleState)
+
+
+
+
+  return (
+
+
+
+    <div className='test'>
+
+        <div className='row0'>
+            <div className='row0-t0 hoverChange triangle-border-white' onClick={() => {setTriangleState(triangleState => !triangleState)}}></div>
+            <div className='row0-t1 hoverChange triangle-border-white'></div>
+            <div className='row0-t2 hoverChange triangle-border-white'></div>
+            <div className='row0-t3 hoverChange triangle-border-white'></div>
+            <div className='row0-t4 hoverChange triangle-border-white'></div>
+            <div className='row0-t5 hoverChange triangle-border-white'></div>
+            <div className='row0-t6 hoverChange triangle-border-white'></div>
+            <div className='row0-t7 hoverChange triangle-border-white'></div>
+            <div className='row0-t8 hoverChange triangle-border-white'></div>
+            <div className='row0-t9 hoverChange triangle-border-white'></div>
+        </div>
+
+        <div className='row1'>
+            <div className='row1-t0 hoverChange triangle-border-black'></div>
+            <div className='row1-t1 hoverChange triangle-border-black'></div>
+            <div className='row1-t2 hoverChange triangle-border-black'></div>
+            <div className='row1-t3 hoverChange triangle-border-black'></div>
+            <div className='row1-t4 hoverChange triangle-border-black'></div>
+            <div className='row1-t5 hoverChange triangle-border-black'></div>
+            <div className='row1-t6 hoverChange triangle-border-black'></div>
+            <div className='row1-t7 hoverChange triangle-border-black'></div>
+            <div className='row1-t8 hoverChange triangle-border-black'></div>
+            <div className='row1-t9 hoverChange triangle-border-black'></div>
+        </div>
+
+        <div className='row2'>
+            <div className='row2-t0 hoverChange triangle-border-white'></div>
+            <div className='row2-t1 hoverChange triangle-border-white'></div>
+            <div className='row2-t2 hoverChange triangle-border-white'></div>
+            <div className='row2-t3 hoverChange triangle-border-white'></div>
+            <div className='row2-t4 hoverChange triangle-border-white'></div>
+            <div className='row2-t5 hoverChange triangle-border-white'></div>
+            <div className='row2-t6 hoverChange triangle-border-white'></div>
+            <div className='row2-t7 hoverChange triangle-border-white'></div>
+            <div className='row2-t8 hoverChange triangle-border-white'></div>
+            <div className='row2-t9 hoverChange triangle-border-white'></div>
+        </div>
+
+        <div className='row3'>
+            <div className='row3-t0 hoverChange triangle-border-black'></div>
+            <div className='row3-t1 hoverChange triangle-border-black'></div>
+            <div className='row3-t2 hoverChange triangle-border-black'></div>
+            <div className='row3-t3 hoverChange triangle-border-black'></div>
+            <div className='row3-t4 hoverChange triangle-border-black'></div>
+            <div className='row3-t5 hoverChange triangle-border-black'></div>
+            <div className='row3-t6 hoverChange triangle-border-black'></div>
+            <div className='row3-t7 hoverChange triangle-border-black'></div>
+            <div className='row3-t8 hoverChange triangle-border-black'></div>
+            <div className='row3-t9 hoverChange triangle-border-black'></div>
+        </div>
+
+        <div className='row4'>
+            <div className='row4-t0 hoverChange triangle-border-white'></div>
+            <div className='row4-t1 hoverChange triangle-border-white'></div>
+            <div className='row4-t2 hoverChange triangle-border-white'></div>
+            <div className='row4-t3 hoverChange triangle-border-white'></div>
+            <div className='row4-t4 hoverChange triangle-border-white'></div>
+            <div className='row4-t5 hoverChange triangle-border-white'></div>
+            <div className='row4-t6 hoverChange triangle-border-white'></div>
+            <div className='row4-t7 hoverChange triangle-border-white'></div>
+            <div className='row4-t8 hoverChange triangle-border-white'></div>
+            <div className='row4-t9 hoverChange triangle-border-white'></div>
+        </div>
+
+        <div className='row5'>
+            <div className='row5-t0 hoverChange triangle-border-black'></div>
+            <div className='row5-t1 hoverChange triangle-border-black'></div>
+            <div className='row5-t2 hoverChange triangle-border-black'></div>
+            <div className='row5-t3 hoverChange triangle-border-black'></div>
+            <div className='row5-t4 hoverChange triangle-border-black'></div>
+            <div className='row5-t5 hoverChange triangle-border-black'></div>
+            <div className='row5-t6 hoverChange triangle-border-black'></div>
+            <div className='row5-t7 hoverChange triangle-border-black'></div>
+            <div className='row5-t8 hoverChange triangle-border-black'></div>
+            <div className='row5-t9 hoverChange triangle-border-black'></div>
+        </div>
+
+        <div className='row6'>
+            <div className='row6-t0 hoverChange triangle-border-white'></div>
+            <div className='row6-t1 hoverChange triangle-border-white'></div>
+            <div className='row6-t2 hoverChange triangle-border-white'></div>
+            <div className='row6-t3 hoverChange triangle-border-white'></div>
+            <div className='row6-t4 hoverChange triangle-border-white'></div>
+            <div className='row6-t5 hoverChange triangle-border-white'></div>
+            <div className='row6-t6 hoverChange triangle-border-white'></div>
+            <div className='row6-t7 hoverChange triangle-border-white'></div>
+            <div className='row6-t8 hoverChange triangle-border-white'></div>
+            <div className='row6-t9 hoverChange triangle-border-white'></div>
+        </div>
+
+        <div className='row7'>
+            <div className='row7-t0 hoverChange triangle-border-black'></div>
+            <div className='row7-t1 hoverChange triangle-border-black'></div>
+            <div className='row7-t2 hoverChange triangle-border-black'></div>
+            <div className='row7-t3 hoverChange triangle-border-black'></div>
+            <div className='row7-t4 hoverChange triangle-border-black'></div>
+            <div className='row7-t5 hoverChange triangle-border-black'></div>
+            <div className='row7-t6 hoverChange triangle-border-black'></div>
+            <div className='row7-t7 hoverChange triangle-border-black'></div>
+            <div className='row7-t8 hoverChange triangle-border-black'></div>
+            <div className='row7-t9 hoverChange triangle-border-black'></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Test
