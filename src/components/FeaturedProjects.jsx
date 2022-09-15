c13cd260a334400920f9d97d1225ffc80e31205e
MichaelGiresi <mikegiresidev@gmail.com>
2022-09-15 16:18:02 -0400

    featured projects jsx cleaned

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -14,7 +14,8 @@ const FeaturedProjects = () => {
           <h4 className='fp-info1-year'>2022</h4>
         </div>
         <div className='fp-info1-info'>
-          <p className='fp-info1-info-p'>Matchbox Stickers is a limited quanity e-commerce website. <span id="mbs-info-span">Talented artists from around the world sell a limited quanity of their creations.</span></p>
+          <p className='fp-info1-info-p'>Matchbox Stickers is a limited quanity e-commerce website. <span id="mbs-info-span">Talented artists from around the world sell a limited quanity of their creations.</span>
+          </p>
           <div id="fp-info1-roles-container">
             <p className='fp-info1-info-roles'>MY ROLE<br /><strong>Development</strong></p>
             <p className='fp-info1-info-roles'>ALEX SEVERINO<br/><strong>Design</strong></p>
@@ -22,7 +23,8 @@ const FeaturedProjects = () => {
         </div>
       </div>
       <div className='fp-showcase-container'>
-        <div className='fp-showcase-1' id="fp-showcase-mbs-1"><a href='https://stupendous-puppy-0e318b.netlify.app' rel='noopener' target = '_blank' className='fp-showcase-button'>VISIT THE SITE</a></div>
+        <div className='fp-showcase-1' id="fp-showcase-mbs-1"><a href='https://stupendous-puppy-0e318b.netlify.app' rel='noopener' target = '_blank' className='fp-showcase-button'>VISIT THE SITE</a>
+        </div>
         <div className='fp-showcase-2' id="fp-showcase-mbs-2"></div>
       </div>
       <div className='nav_separator-fp'></div>
@@ -40,7 +42,8 @@ const FeaturedProjects = () => {
         </div>
       </div>
       <div className='fp-showcase-container'>
-        <div className='fp-showcase-1' id="fp-showcase-algo-1"><a href='https://famous-dango-24aba4.netlify.app' rel='noopener' target = '_blank' className='fp-showcase-button'>VISIT THE SITE</a></div>
+        <div className='fp-showcase-1' id="fp-showcase-algo-1"><a href='https://famous-dango-24aba4.netlify.app' rel='noopener' target = '_blank' className='fp-showcase-button'>VISIT THE SITE</a>
+        </div>
         <div className='fp-showcase-2' id="fp-showcase-algo-2"></div>
       </div>
     </div>
